09d8a66d84c73f65ee4f0114967fc0c3c8809914
Jürgen Viertbauer <juergen.viertbauer@vb-data.at>
2020-04-04 16:03:13 +0200

    Projektdateien hinzufügen.

---
+++ TelnetServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkLibrary;
+using NetworkLibrary.EventArgs;
+
+namespace TelnetServer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //definiere einen Endpoint für den Listener 
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, 23000);
+
+            //initialisiere Netzwerk Listener
+            var Listener = new AsyncSocketListener(ep);
+
+            //gibt empfangenen Text auf der Konsole aus
+            Listener.ClientConnectedHandler += GiveOutText;
+
+            //Starte Listen
+            await Listener.Listen();
+
+        }
+
+        static void GiveOutText(object sender, ClientConnectedEventArgs e)
+        {
+            Console.WriteLine(e.message);
+        }
+    }
+}
